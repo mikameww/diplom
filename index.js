4f6a17b1339026806855a7b6a5cd497cc721d6ee
mikameww <152682789+mikameww@users.noreply.github.com>
2024-05-18 15:00:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var colorPicker = document.getElementById("bgcolor");
 
+function changebodycolor(){
+   var colorVal = colorPicker.value;
+   document.body.style.background = colorVal;
+}
+
+colorPicker.addEventListener("change",
+changebodycolor,false);
